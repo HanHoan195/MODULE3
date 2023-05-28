efa5da184c6cf5e5521d4223af4a5a85a5588e0b
HanHoan195 <124326008+HanHoan195@users.noreply.github.com>
2023-05-28 21:57:51 +0700

    Update [bt]truyvandulieu.sql

--- thao tác với CSDL/[bt]truyvandulieu.sql
+++ thao tác với CSDL/[bt]truyvandulieu.sql
@@ -11,3 +11,6 @@ select * from subject where credit between 3 and 5;
 update student set classid = 2 where studentname = 'hung' ;
 
 -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select s.studentname, sub.subname, m.mark
+from student s join mark m on s.studentid = m.studentid
+join subject sub on sub.subid = m.subid;
