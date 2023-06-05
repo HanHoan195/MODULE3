b4bb0a75adda4aee168d5542e99b5b024a029f20
HanHan195 <henrytran.195@gmail.com>
2023-06-05 15:17:10 +0700

    doi tien

---
+++ Java-web-currency-converter/src/main/java/com/example/javawebcurrencyconverter/HelloServlet.java
@@ -0,0 +1,39 @@
+package com.example.javawebcurrencyconverter;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+    @WebServlet(name = "ConverterServlet", value= "/convert")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("convert.jsp").forward(request, response);
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        float rate = Float.parseFloat(req.getParameter("rate"));
+        float usd = Float.parseFloat(req.getParameter("usd"));
+
+        float vnd = rate * usd;
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Rate: " + rate + "</h1>");
+        writer.println("<h1>USD: " + usd + "</h1>");
+        writer.println("<h1>VND: " + vnd + "</h1>");
+        writer.println("</html>");
+
+
+    }
+}
