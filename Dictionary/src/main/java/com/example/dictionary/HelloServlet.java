35874b2b6bacc96507b4fd1bfbede4df7ddd1c4d
HanHan195 <henrytran.195@gmail.com>
2023-06-05 16:21:46 +0700

    dictionary

---
+++ Dictionary/src/main/java/com/example/dictionary/HelloServlet.java
@@ -0,0 +1,44 @@
+package com.example.dictionary;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "TranslateServlet", value = "/translate")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+        String search = req.getParameter("txtSearch");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        String result = dictionary.get(search);
+        if (result != null) {
+            writer.println("Word: " + search + "\n");
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+        writer.println("</html>");
+    }
+}
